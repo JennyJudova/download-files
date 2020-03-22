e5397699f0d55b08f928fef88a73858df36b5071
JennyJudova <jjudova@gmail.com>
2020-03-22 22:57:13 +0000

    status added

--- app/helpers/selected-counter.js
+++ app/helpers/selected-counter.js
@@ -1,8 +1,9 @@
 import { helper } from "@ember/component/helper";
 
 export default helper(function selectedCounter(params /*, hash*/) {
-  const files = params;
-  const filesTrue = files.filter(file => file.isChecked === true);
-  const counter = filesTrue.length;
-  return counter;
+  if (params[0] === 0) {
+    return "None Selected";
+  } else {
+    return `Selected ${params[0]}`;
+  }
 });
